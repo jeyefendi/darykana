0348ae684e05c9193ca8d7bd292b04796f46defa
jeyefendi <jeyhunfarajov@gmail.com>
2022-07-15 17:55:30 +0300

    complete Header component

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import styles from "../styles/Header.module.css";
+import { TbPill } from "react-icons/tb";
+
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <nav>
+      <TbPill size="3em"/>
+      <ul className={styles.nav}>
+        <li className={styles.tab}><a href="#">Сотрудничество</a></li>
+        <li className={styles.tab}><a href="#">Войти</a></li>
+      </ul>
+    </nav>
+  );
+};
 
-export default Header
+export default Header;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
-import Hero from '../components/Hero' 
+import Hero from "../components/Hero";
+import Header from "../components/Header";
 
 export default function Home() {
   return (
     <div>
-      <Hero/>
+      <Header />
+      <Hero />
     </div>
-  )
+  );
 }
