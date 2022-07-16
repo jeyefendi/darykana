5d19739afe477218db1381cb5b33e3237c857047
jeyefendi <jeyhunfarajov@gmail.com>
2022-07-16 21:50:04 +0300

    add head (title)

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import Hero from "../components/Hero";
 import Header from "../components/Header";
+import Head from "../components/Head";
+
 
 export default function Home() {
   return (
     <div>
+      <Head />
       <Header />
       <Hero />
     </div>
